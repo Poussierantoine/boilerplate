22bcea364dbbe3fc8cc775becac6456a354318aa
Antoine Poussier <antoine.poussier@etu.univ-grenoble-alpes.fr>
2023-09-17 12:11:31 +0200

    adding vscode shortcuts and settings, webpack, eslint configs and basic package.json

---
+++ web-app/webpack.config.js
@@ -0,0 +1,130 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === "dev";
+
+// PLUGINS
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+// PATHS
+const dirApp = path.join(__dirname, "app");
+const dirShared = path.join(__dirname, "shared");
+const dirStyles = path.join(__dirname, "styles");
+const dirNode = "node_modules";
+
+// CONFIG
+module.exports = {
+  entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
+
+  resolve: {
+    modules: [dirApp, dirShared, dirStyles, dirNode],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT,
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./shared",
+          to: "",
+        },
+      ],
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+
+      {
+        test: /\.(png|jpe?g|gif|svg|ico|woff2?|fnt|webp)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: ImageMinimizerPlugin.loader,
+            options: {
+              minimizer: {
+                implementation: ImageMinimizerPlugin.imageminMinify,
+                options: {
+                  plugins: [
+                    "imagemin-gifsicle",
+                    "imagemin-jpegtran",
+                    "imagemin-optipng",
+                  ],
+                },
+              },
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.(glsl|frag|vert)$/,
+        type: "asset/source",
+        loader: "glslify-loader",
+        exclude: /node_modules/,
+      },
+
+    ],
+  },
+
+  optimization: {
+    minimizer: [
+      "...",
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            // Lossless optimization with custom option
+            // Feel free to experiment with options for better result for you
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+};
